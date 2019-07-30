035ec7a77d9ae64d3ffe6fee28b5c1c8ed6a531b
Jiovani Ledesma Arredondo <jiovaniledesmaa@gmail.com>
2019-07-30 00:26:38 -0500

    Actualizado

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+# Current status: pre-alpha
+
+setup(name='SGfRobots',
+      version='0.1.0',
+      description='Python library for Solids Generator for Robots',
+      author='J.L',
+      author_email='jiovaniledesmaa@gmail.com',
+      license = "MIT",
+      keywords=["Robotics","Solids", "Generator"],
+      url='https://github.com/JiovaniLedesma/SGfRobots',
+      packages=['SGfRobots'],
+      classifiers=[
+      "Development Status :: 2 - Pre-Alpha",
+      "Intended Audience :: Education",
+      "Intended Audience :: Developers",
+      "License :: OSI Approved :: MIT License",
+      "Operating System :: OS Independent",
+      "Programming Language :: Python",
+      "Programming Language :: Python :: 3.6",
+      "Programming Language :: Python :: Implementation :: CPython",
+      ]
+      )
